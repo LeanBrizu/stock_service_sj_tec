390c6e67d26ef75467f0461cacbcbdfa9bcbfc41
LeanBrizu <leandrobri.ar@gmail.com>
2023-05-01 18:41:13 -0300

    Conformar rutas para el funcionamiento de los métodos en InicioController.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\InicioController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [InicioController::class, 'index'])->name('inicio.index');
+Route::get('/', [InicioController::class, 'index'])->name('inicio');
+Route::post('/', [InicioController::class, 'guardarMensajeContacto'])->name('guardarMensajeContacto');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
