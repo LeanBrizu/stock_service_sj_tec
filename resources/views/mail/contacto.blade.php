4e351bc82ff607ad54a95627b5a295b8b073743e
LeanBrizu <leandrobri.ar@gmail.com>
2023-05-01 18:37:31 -0300

    Crear y conformar vista del correo que reciben quienes intentan contactarse.

---
+++ resources/views/mail/contacto.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<title>Gracias por contactarnos.</title>
+</head>
+<body>
+<h1>Hemos recibido tu mensaje.</h1>
+<p> Hola {{ $detalles['Nombre']}}, gracias por comunicarte con nosotros. Mantente atento a nuestra pronta respuesta.</p>
+</body>
+</html>
+
