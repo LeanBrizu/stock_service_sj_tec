712f55b1d722ad8fd1eb7e20d6cd6bb5e6588413
LeanBrizu <leandrobri.ar@gmail.com>
2023-05-02 20:33:35 -0300

    Conformar formulario de contacto en la URL raíz.

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -4,27 +4,115 @@
   <h1>Contáctanos</h1>
   <form action="{{ route('guardarMensajeContacto') }}" method="POST">
     @csrf
-    <div class="form-group">
-      <label for="Nombre">Nombre</label>
-      <input type="text" name="Nombre" id="Nombre" class="form-control" required>
+    <div>
+        <h2>Información Personal</h2>
+        <div class="">
+          <label for="Nombre">Nombre</label>
+          <input type="text" name="Nombre" id="Nombre" class="form-control" required>
+        </div>
+        <div class="">
+          <label for="Correo">Email</label>
+          <input type="email" name="Correo" id="Correo" class="form-control" required>
+        </div>
+        <div class="">
+          <label for="Telefono">Teléfono</label>
+          <input type="tel" name="Telefono" id="Telefono" class="form-control" rows="5" required></textarea>
+        </div>
+        <div class="">
+          <label for="Mensaje">Mensaje</label>
+          <textarea name="Mensaje" id="Mensaje" class="form-control" rows="5" required></textarea>
+        </div>
     </div>
-    <div class="form-group">
-      <label for="Correo">Email</label>
-      <input type="email" name="Correo" id="Correo" class="form-control" required>
+    <div>
+        <h2>Información de la Propiedad</h2>
+        <div>
+            <label for="venta">
+            <h3>Tipo de operación:</h3>
+            </label>
+            <div>
+                <input type="radio" name="Operacion" id="venta" value="Venta" checked>
+                <label for="venta">
+                    Venta
+                </label>
+            </div>
+            <div>
+                <input type="radio" name="Operacion" id="compra" value="Compra">
+                <label for="compra">
+                    Compra
+                </label>
+            </div>
+        </div>
+        <div>
+        <hr>
+            <label for="precio"><h3>Precio o Presupuesto:</h3></label>
+            <div>
+                <span><b>$</b></span>
+                <input type="text" class="form-precio" id="precio" max="1000000000" min="0" name="Precio" aria-describedby="ayudaPrecio" required>
+            </div>
+            <small id="ayudaPrecio">
+                Ingrese el precio o presupuesto en dólares estadounidenses. (US$). Máximo 1.000.000.000.
+            </small>
+        </div>
+        <hr>
+        <div>
+           <label for="tipo-contacto">
+            <h3>¿Cómo desea ser contactado?:</h3>
+            </label>
+            <div>
+                <input type="radio" name="Tipo_contacto" id="telefono" value="Teléfono" checked>
+                <label for="tipo-contacto">
+                    Teléfono
+                </label>
+            </div>
+            <div>
+                <input type="radio" name="Tipo_contacto" id="email" value="Correo Electrónico">
+                <label for="compra">
+                    Correo Electrónico
+                </label>
+            </div>
+        </div>
     </div>
-    <div class="form-group">
-      <label for="Mensaje">Mensaje</label>
-      <textarea name="Mensaje" id="Mensaje" class="form-control" rows="5" required></textarea>
+    <div>
+        <h2>Atención de la inmobiliaria</h2>
+        <label for="personal-inmobiliario">
+        Seleccione quien le brindará atención:
+        </label>
+        <select id="personal-inmobiliario">
+            <option value="Evaristo Artigas Colom">Evaristo Artigas Colom</option>
+            <option value="Josefa Cerdán Montserrat">Josefa Cerdán Montserrat</option>
+            <option value="Celestino Seve Aranda Goñi">Celestino Seve Aranda Goñi</option>
+        </select>
     </div>
-    <button type="submit" class="btn btn-primary">Enviar</button>
+    <br>
+    <button type="submit" class="btn-primary">Enviar</button>
   </form>
 </div>
+
+<script>
+document.addEventListener('DOMContentLoaded', () => {
+  const precioInput = document.getElementById('precio');
+
+  precioInput.addEventListener('input', e => {
+    let value = e.target.value;
+    value = value.replace(/[^\d]/g, '');
+    if (value < 0 || value > 1000000000) {
+      value = '';
+    }
+    e.target.value = value;
+  });
+});
+</script>
 <style>
   .container {
     height: 50%;
-    max-height: 50%
-    max-width: 50%;
-    width: 50%;
+    margin: auto;
+    max-height: 50%;
+    max-width: 40%;
+    width: 40%;
+  }
+
+  form input, textarea, select, option {
+    font-size: 16px;
   }
 
   form label {
@@ -36,6 +124,12 @@
   .form-control {
     width: 100%;
     padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    margin-bottom: 20px;
+  }
+
+  .form-precio {
     border: 1px solid #ccc;
     border-radius: 5px;
     margin-bottom: 20px;
