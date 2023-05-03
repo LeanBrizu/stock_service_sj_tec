325a2d909f45d71d8257f8216303ffa32c6e9fda
LeanBrizu <leandrobri.ar@gmail.com>
2023-05-03 01:12:39 -0300

    Conformar vista de email con los datos del formulario de contacto.

--- resources/views/mail/contacto.blade.php
+++ resources/views/mail/contacto.blade.php
@@ -7,14 +7,14 @@
 <body>
 <h1>Futuro cliente - Asesoramientos inmobiliarios.</h1>
 <h2>Información Personal</h2>
-<p><b>Nombre: </b>{{ isset($Nombre) ? $Nombre : '(Sin datos).' }}</p>
-<p><b>Email: </b>{{ isset($Correo) ? $Correo : '(Sin datos).' }}</p>
-<p><b>Teléfono: </b>{{ isset($Telefono) ? $Telefono : '(Sin datos).' }}</p>
-<p><b>Mensaje: </b>{{ isset($Mensaje) ? $Mensaje : '(Sin datos).' }}</p>
+<p><b>Nombre: </b>{{ isset($detalles['Nombre']) ? $detalles['Nombre'] : '(Sin datos).' }}</p>
+<p><b>Email: </b>{{ isset($detalles['Correo']) ? $detalles['Correo'] : '(Sin datos).' }}</p>
+<p><b>Teléfono: </b>{{ isset($detalles['Telefono']) ? $detalles['Telefono'] : '(Sin datos).' }}</p>
+<p><b>Mensaje: </b>{{ isset($detalles['Mensaje']) ? $detalles['Mensaje'] : '(Sin datos).' }}</p>
 <h2>Información de la Propiedad</h2>
-<p><b>Tipo de operación: </b>{{ isset($Operacion) ? $Operacion : '(Sin datos).' }}</p>
-<p><b>Precio o Presupuesto: </b>{{ isset($Precio) ? $Precio : '(Sin datos).' }}</p>
-<p><b>¿Cómo desea ser contactado?: </b>{{ isset($Tipo_contacto) ? $Tipo_contacto : '(Sin datos).' }}</p>
+<p><b>Tipo de operación: </b>{{ isset($detalles['Operacion']) ? $detalles['Operacion'] : '(Sin datos).' }}</p>
+<p><b>Precio o Presupuesto: </b>{{ isset($detalles['Precio']) ? $detalles['Precio'] : '(Sin datos).' }}</p>
+<p><b>¿Cómo desea ser contactado?: </b>{{ isset($detalles['Tipo_contacto']) ? $detalles['Tipo_contacto'] : '(Sin datos).' }}</p>
 <h2>Atención de la inmobiliaria</h2>
 <p>¡Felicitaciones! Ha sido seleccionado por el cliente para brindarles atención personalizada.</p>
 </body>
